99214d662ab389e14f8c3bd0ae2349bba1f04991
lkania <lukasz.kania.cc@gmail.com>
2020-05-02 10:52:58 +0200

    Add results printing

--- automated.m
+++ automated.m
@@ -15,13 +15,14 @@ Z_input = [8 11 4];
 A_input = [-5 -7 0; -3 0 -7];
 b_input = [-5; -4];
 [Xopt, Zopt, costraint, i] = linprog_new_constraints(Z_input, A_input, b_input);
-
+Results = [];
 for main_loop= 1:2
     [A_main, b_main] = calculate_parameters(main_loop, i, costraint, A_input, b_input);
     [Xopt, Zopt, main_costraint, j] = linprog_new_constraints(Z_input, A_main, b_main);
     if isempty(Xopt)
         continue
     elseif isempty(main_costraint)
+        Results=[Results; Zopt, Xopt];
         sprintf('x1:%.2f, x2:%.2f, x3:%.2f, Z:%.2f',Xopt(1),Xopt(2),Xopt(3),Zopt)
         continue
     end
@@ -32,6 +33,7 @@ for main_loop= 1:2
             continue
         elseif isempty(second_costraint)
             sprintf('x1:%.2f, x2:%.2f, x3:%.2f, Z:%.2f',Xopt(1),Xopt(2),Xopt(3),Zopt)
+            Results=[Results; Zopt, Xopt];
             continue
         end
         for third_loop = 1:2
@@ -40,9 +42,12 @@ for main_loop= 1:2
             if isempty(Xopt)
                 continue
             elseif isempty(third_costraint)
+                Results=[Results; Zopt, Xopt];
                 sprintf('x1:%.2f, x2:%.2f, x3:%.2f, Z:%.2f',Xopt(1),Xopt(2),Xopt(3),Zopt)
                 continue
             end         
         end
     end
 end
+[M, I] = min(Results(:,1));
+sprintf('The optimal solution is: x1:%.2f, x2:%.2f, x3:%.2f, Z:%.2f', Results(I,2),Results(I,3),Results(I,4),Results(I,1))

64eae9c89ced98b7e2ce9e67e6d2dd71d39a9598
lkania <lukasz.kania.cc@gmail.com>
2020-06-07 23:30:06 +0200

    Add plot results to file, for multiple parameters

--- lab_linear_quadratic_something/python_scripts/linearquadratic.py
+++ lab_linear_quadratic_something/python_scripts/linearquadratic.py
@@ -1,10 +1,11 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class LinearQuadraticProblem:
 
-    def __init__(self, N=100, F=10.0, Q=9, A=1, B=1, R=15, x_0=10,
-                 is_finite=True, eps=0.0001):
+    def __init__(self, N=100, F=10.0, A=1, B=1, x_0=10, eps=0.0001, Q=9, R=15,
+                 is_finite=False):
         self.N = N
         self.F = F
         self.Q = Q
@@ -17,7 +18,7 @@ class LinearQuadraticProblem:
         self.J = 0
         self.is_finite = is_finite
         self.eps = eps
-        self.number_of_iterations = N
+        self.no_of_iterations = N
 
     def calculate_k(self):
         i = self.N - 2
@@ -29,7 +30,7 @@ class LinearQuadraticProblem:
 
             if not self.is_finite:
                 if abs(self.K[0] - self.K[1]) < self.eps:
-                    self.number_of_iterations = self.N - i
+                    self.no_of_iterations = self.N - i
                     break
             i -= 1
 
@@ -37,9 +38,25 @@ class LinearQuadraticProblem:
         self.J = 0.5 * self.x[0] * self.K[1] * self.x[0]
 
     def calculate_control_and_state(self):
-        for i in range(self.number_of_iterations - 2):
-            self.u.append(-(1/(self.R + self.B *
-                        self.K[i + 1] * self.B)) * self.B * self.K[i + 1] *
-                        self.A * self.x[i])
+        for i in range(self.no_of_iterations - 2):
+            self.u.append(-(1/(self.R + self.B * self.K[i + 1] * self.B)) *
+                          self.B * self.K[i + 1] * self.A * self.x[i])
             self.x.append(self.A * self.x[i] + self.B * self.u[i])
+        self.u.append(-(1 / (self.R + self.B * self.K[self.no_of_iterations - 2]
+                             * self.B)) * self.B *
+                      self.K[self.no_of_iterations - 2] * self.A *
+                      self.x[self.no_of_iterations - 2])
 
+    def plot_results(self):
+        plt.subplot(121)
+        plt.plot(self.u)
+        plt.xlabel("iteration")
+        plt.ylabel("value")
+        plt.title("control variable, Q = " + str(self.Q) + ", R =" + str(self.R))
+        plt.subplot(122)
+        plt.plot(self.x)
+        plt.xlabel("iteration")
+        plt.title("trajectory, finite = " + str(self.is_finite))
+        # plt.show()
+        plt.savefig("finite_{}_Q_{}_R_{}".format(self.is_finite, self.Q, self.R))
+        plt.close()
--- lab_linear_quadratic_something/python_scripts/main.py
+++ lab_linear_quadratic_something/python_scripts/main.py
@@ -1,12 +1,14 @@
 from linearquadratic import LinearQuadraticProblem as LQP
 
-
 def main():
-    problem = LQP()
-    problem.calculate_k()
-    problem.calculate_performance_index()
-    problem.calculate_control_and_state()
-    dupa = 123
+    _is_finite = False
+    for _Q in [3, 6, 9]:
+        for _R in [2, 7, 11]:
+                problem = LQP(Q=_Q, R=_R, is_finite = _is_finite)
+                problem.calculate_k()
+                problem.calculate_performance_index()
+                problem.calculate_control_and_state()
+                problem.plot_results()
 
 
 if __name__ == "__main__":
